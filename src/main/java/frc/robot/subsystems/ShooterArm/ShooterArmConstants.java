7852da4e5ff2015145aa6fa01fae0b55f185d382
ElijahLanman <122069192+ElijahLanman@users.noreply.github.com>
2025-11-22 12:54:50 -0800

    Create Shooter Arm Config

--- src/main/java/frc/robot/subsystems/ShooterArm/ShooterArmConstants.java
+++ src/main/java/frc/robot/subsystems/ShooterArm/ShooterArmConstants.java
@@ -1,6 +1,32 @@
 package frc.robot.subsystems.ShooterArm;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 public class ShooterArmConstants {
      
     public static final int motorID = 41;
+
+    public static final TalonFXConfiguration kConfig = new TalonFXConfiguration();
+    static {
+        MotorOutputConfigs output = kConfig.MotorOutput;
+        output.NeutralMode = NeutralModeValue.Brake;
+
+        CurrentLimitsConfigs current = kConfig.CurrentLimits;
+        current.StatorCurrentLimitEnable = true;
+        current.StatorCurrentLimit = 40; 
+
+        Slot0Configs control = kConfig.Slot0;
+        control.kP = 0.5; 
+        control.kI = 0;
+        control.kD = 0;
+        
+        control.kS = 0.1;
+        control.kV = 0;
+        control.kA = 0;
+    }
 }
