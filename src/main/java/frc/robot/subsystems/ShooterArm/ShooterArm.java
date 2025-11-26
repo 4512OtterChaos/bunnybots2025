93c2d3b9115f331106d94d7174bd838b7a27819d
4512 Otter Chaos <otterchaos4512@gmail.com>
2025-11-25 18:02:09 -0800

    apply ShooterArm configuration

--- src/main/java/frc/robot/subsystems/ShooterArm/ShooterArm.java
+++ src/main/java/frc/robot/subsystems/ShooterArm/ShooterArm.java
@@ -1,14 +1,25 @@
 package frc.robot.subsystems.ShooterArm;
 
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import static edu.wpi.first.wpilibj2.command.Commands.*;
 
 import static frc.robot.subsystems.ShooterArm.ShooterArmConstants.*; 
 
 public class ShooterArm {
     
-    TalonFX motor = new TalonFX(motorID);
+    TalonFX leftMotor = new TalonFX(kMotorID);
+    TalonFX rightMotor = new TalonFX(kMotorID);
+
+    public ShooterArm() {
+        leftMotor.getConfigurator().apply(kConfig);
+        rightMotor.getConfigurator().apply(kConfig);
+        rightMotor.setControl(new Follower(leftMotor.getDeviceID(), true));
+    }
     
     public void setVoltage(double voltage) {
-        motor.setVoltage(voltage);
+        leftMotor.setVoltage(voltage);
     }
 }
