e18f19de5b8a9957af7004c7b2c9f58a1703ab94
4512 Otter Chaos <otterchaos4512@gmail.com>
2025-12-04 17:31:19 -0800

    Basic Shot Sequence

---
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -0,0 +1,34 @@
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.wpilibj2.command.Commands.*;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Funnel.Funnel;
+import frc.robot.subsystems.Intake.Intake;
+import frc.robot.subsystems.ShooterArm.ShooterArm;
+import frc.robot.subsystems.ShooterWheel.ShooterWheels;
+
+public class Superstructure {
+    Intake intake;
+    ShooterArm shooterArm;
+    ShooterWheels shooterWheels;
+    Funnel funnel; 
+
+    public Superstructure (Intake intake, ShooterArm shooterArm, ShooterWheels shooterWheels, Funnel funnel) {
+        this.intake = intake;
+        this.shooterArm = shooterArm;
+        this.shooterWheels = shooterWheels;
+        this.funnel = funnel;
+    } 
+
+    public Command shotSequence(){
+        return sequence(
+            shooterWheels.setVelocityC(RPM.of(430)),
+            waitUntil(shooterWheels.upToSpeedT()),
+            shooterArm.setVoltageInC(),
+            waitSeconds(0.5)
+        );
+    }
+
+}
