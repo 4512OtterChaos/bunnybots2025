28d385fdfe5e7cf69b73579bdafce580aef089ec
ElijahLanman <122069192+ElijahLanman@users.noreply.github.com>
2025-12-08 22:17:14 -0800

    Funnel Simulation

--- src/main/java/frc/robot/subsystems/Funnel/Funnel.java
+++ src/main/java/frc/robot/subsystems/Funnel/Funnel.java
@@ -1,15 +1,32 @@
 package frc.robot.subsystems.Funnel;
 
+import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Degrees;
-import static edu.wpi.first.wpilibj2.command.Commands.runOnce;
+import static edu.wpi.first.units.Units.KilogramSquareMeters;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.Volts;
 import static frc.robot.subsystems.Funnel.FunnelConstants.*;
 
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.sim.ChassisReference;
+import com.ctre.phoenix6.sim.TalonFXSimState;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -32,7 +49,12 @@ public class Funnel extends SubsystemBase{
 
     @Override
     public void periodic() {
-        
+        // ### Simulation
+
+        visualizeState(leftMotor.getPosition().getValue());
+        visualizeSetpoint(targetAngle);
+
+        log();
     }
 
     // public void setVoltage(double voltage) {
@@ -54,4 +76,93 @@ public class Funnel extends SubsystemBase{
     }
 
 
+    
+    //########## Simulation (IGNORE FOR NOW)
+
+    public void log() {
+        SmartDashboard.putNumber("Funnel/Funnel Native", leftMotor.getPosition().getValueAsDouble());
+        SmartDashboard.putNumber("Funnel/Funnel Degrees", leftMotor.getPosition().getValue().in(Degrees));
+        SmartDashboard.putNumber("Funnel/Funnel Target Degrees", targetAngle.in(Degrees));
+        SmartDashboard.putNumber("Funnel/Motor Current", leftMotor.getStatorCurrent().getValue().in(Amps));
+        // SmartDashboard.putBoolean("Funnel/isStalled", isStalled());
+        SmartDashboard.putNumber("Funnel/Motor Voltage", leftMotor.getMotorVoltage().getValue().in(Volts));
+        // SmartDashboard.putNumber("Funnel/Motor Target Voltage", targetVoltage);
+        SmartDashboard.putNumber("Funnel/Motor Velocity", leftMotor.getVelocity().getValue().in(RPM));
+        // SmartDashboard.putBoolean("Funnel/Motor Stalled", isStalled());
+        SmartDashboard.putNumber("Funnel/Time", Timer.getFPGATimestamp());
+        SmartDashboard.putData("Funnel/Mech2d", mech);
+    }
+    
+    Mechanism2d mech = new Mechanism2d(.8, .8, new Color8Bit(0, 100, 150));
+    MechanismRoot2d mechRoot = mech.getRoot("funnel", 0.4, 0.1);
+
+    private final Color8Bit kSetpointBaseColor = new Color8Bit(150, 0, 0);
+    private final Color8Bit kMechBaseColor = new Color8Bit(0, 0, 150);
+
+    private final double kSetpointWidth = 6;
+    private final double kMechWidth = 9;
+
+    private final double kDefaultFunnelDeg = 90;
+
+    private final MechanismLigament2d mechStart = mechRoot.append(
+            new MechanismLigament2d("Arm", kPivotHeight.in(Meters), 90, kMechWidth, kMechBaseColor));
+    private final MechanismLigament2d mechFunnel = mechStart.append(
+            new MechanismLigament2d("Intake", kFunnelLength.in(Meters), kDefaultFunnelDeg, kMechWidth, kMechBaseColor));
+    
+    private final MechanismLigament2d setpointStart = mechRoot.append(
+            new MechanismLigament2d("setpointArmBase", kPivotHeight.in(Meters), 90, kSetpointWidth, kSetpointBaseColor));
+    private final MechanismLigament2d setpointIntake = setpointStart.append(
+            new MechanismLigament2d("setpointArm", kFunnelLength.in(Meters), kDefaultFunnelDeg, kSetpointWidth,
+                    kSetpointBaseColor));
+    
+    SingleJointedArmSim funnelSim = new SingleJointedArmSim(
+        LinearSystemId.createSingleJointedArmSystem(
+            DCMotor.getKrakenX60(2),
+            kMomentOfInertia.in(KilogramSquareMeters),
+            kGearRatio
+        ),
+        DCMotor.getKrakenX60(2),
+        kGearRatio,
+        kFunnelLength.in(Meters),
+        kHomeAngle.in(Radians),
+        kMaxAngle.in(Radians),
+        true,
+        kHomeAngle.in(Radians));
+
+
+    DCMotorSim motorSim = new DCMotorSim(
+        LinearSystemId.createDCMotorSystem(
+            DCMotor.getKrakenX60(2),
+            kMomentOfInertia.in(KilogramSquareMeters),
+            kGearRatio
+        ),
+        DCMotor.getKrakenX60(2)
+    );
+    
+    public void visualizeState(Angle funnelAngle) {
+        mechFunnel.setAngle(90 - funnelAngle.in(Degrees));
+    }
+    
+    public void visualizeSetpoint(Angle targetAngle) {
+        setpointIntake.setAngle(90 - targetAngle.in(Degrees));
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        TalonFXSimState motorSimState = leftMotor.getSimState();
+        motorSimState.Orientation =  ChassisReference.Clockwise_Positive;//TODO: Fix, idk what it means
+
+        motorSimState.setSupplyVoltage(leftMotor.getSupplyVoltage().getValue());//TODO: Add friction? Also, idk that the voltage should be accessed like this
+
+        motorSim.setInputVoltage(motorSimState.getMotorVoltage());
+
+        motorSim.update(0.02);
+
+        motorSimState.setRawRotorPosition(motorSim.getAngularPositionRotations() * kGearRatio);
+        motorSimState.setRotorVelocity(motorSim.getAngularVelocityRPM() / 60  * kGearRatio);
+        //                                           shaft RPM --> rotations per second --> motor rotations per second
+        double voltage = motorSim.getInputVoltage();
+        funnelSim.setInput(voltage);
+		funnelSim.update(0.02);
+    }
 }
