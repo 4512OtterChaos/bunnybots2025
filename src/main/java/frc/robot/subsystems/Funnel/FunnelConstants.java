e5a9fdccc974c8917f086834a5939c254fd98ed0
ElijahLanman <122069192+ElijahLanman@users.noreply.github.com>
2025-12-08 22:17:14 -0800

    Funnel Simulation

--- src/main/java/frc/robot/subsystems/Funnel/FunnelConstants.java
+++ src/main/java/frc/robot/subsystems/Funnel/FunnelConstants.java
@@ -1,10 +1,17 @@
 package frc.robot.subsystems.Funnel;
 
 import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.InchesPerSecond;
+import static edu.wpi.first.units.Units.Pounds;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
 
+import java.security.PublicKey;
+
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
@@ -13,13 +20,23 @@ import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.MomentOfInertia;
+import static frc.robot.util.OCUnits.*;
 
 public class FunnelConstants {
     public static final int kLeftMotorID = 51;
     public static final int kRightMotorID = 52;
 
+    public static final int kGearRatio = 9; //9:1
+
+    public static final MomentOfInertia kMomentOfInertia = PoundSquareInches.of(425.845093);
+
     public static final Angle kHomeAngle = Degrees.of(0);
-    public static final Angle kMaxAngle = Degrees.of(60);
+    public static final Angle kMaxAngle = Degrees.of(50); //53.97 degrees is the physical maximum (not touching but pretty close)
+
+    public static final Distance kPivotHeight = Inches.of(3.72);
+    public static final Distance kFunnelLength = Inches.of(10.04);
 
     public static final TalonFXConfiguration kConfig = new TalonFXConfiguration();
     static {
