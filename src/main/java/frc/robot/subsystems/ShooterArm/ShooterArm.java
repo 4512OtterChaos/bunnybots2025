d89802beb08f7c280e29e6557fbc96f8ca64f8ea
4512 Otter Chaos <otterchaos4512@gmail.com>
2025-12-11 15:22:09 -0800

    ShooterArm Simultation
    Arms don't have limits

--- src/main/java/frc/robot/subsystems/ShooterArm/ShooterArm.java
+++ src/main/java/frc/robot/subsystems/ShooterArm/ShooterArm.java
@@ -2,10 +2,30 @@ package frc.robot.subsystems.ShooterArm;
 
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.sim.ChassisReference;
+import com.ctre.phoenix6.sim.TalonFXSimState;
 
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.KilogramSquareMeters;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.Volts;
 import static edu.wpi.first.wpilibj2.command.Commands.*;
 
 import static frc.robot.subsystems.ShooterArm.ShooterArmConstants.*; 
@@ -19,6 +39,15 @@ public class ShooterArm extends SubsystemBase{
         leftMotor.getConfigurator().apply(kConfig);
         rightMotor.getConfigurator().apply(kConfig);
         rightMotor.setControl(new Follower(leftMotor.getDeviceID(), true));
+    }
+
+    @Override
+    public void periodic() {
+        //### Simulation
+        visualizeState(leftMotor.getPosition().getValue());
+        visualizeSetpoint(targetVoltage);
+
+        log();
     }
     
     public void setVoltage(double voltage) {
@@ -35,5 +64,106 @@ public class ShooterArm extends SubsystemBase{
 
     public Command setVoltageOutC() {
         return setVoltageC(kVoltageOut).withName("Set Voltage Out");
+    }
+
+
+    
+    //########## Simulation (IGNORE FOR NOW)
+
+    public void log() {
+        SmartDashboard.putNumber("ShooterArm/ShooterArm Native", leftMotor.getPosition().getValueAsDouble());
+        SmartDashboard.putNumber("ShooterArm/ShooterArm Degrees", leftMotor.getPosition().getValue().in(Degrees));
+        SmartDashboard.putNumber("ShooterArm/Motor Current", leftMotor.getStatorCurrent().getValue().in(Amps));
+        // SmartDashboard.putBoolean("Funnel/isStalled", isStalled());
+        SmartDashboard.putNumber("ShooterArm/Motor Voltage", leftMotor.getMotorVoltage().getValue().in(Volts));
+        // SmartDashboard.putNumber("ShooterArm/Target Motor Voltage", targetVoltage);
+        // SmartDashboard.putNumber("Funnel/Motor Target Voltage", targetVoltage);
+        SmartDashboard.putNumber("ShooterArm/Motor Velocity", leftMotor.getVelocity().getValue().in(RPM));
+        // SmartDashboard.putBoolean("Funnel/Motor Stalled", isStalled());
+        SmartDashboard.putNumber("ShooterArm/Time", Timer.getFPGATimestamp());
+        SmartDashboard.putData("ShooterArm/Mech2d", mech);
+    }
+    
+    Mechanism2d mech = new Mechanism2d(.5, .5, new Color8Bit(0, 100, 150));
+    MechanismRoot2d mechRoot = mech.getRoot("ShooterArm", 0.25, 0.05);
+
+    private final Color8Bit kSetpointBaseColor = new Color8Bit(150, 0, 0);
+    private final Color8Bit kMechBaseColor = new Color8Bit(0, 0, 150);
+
+    private final double kSetpointWidth = 6;
+    private final double kMechWidth = 9;
+
+    private final double kDefaultArmDeg = 88.186340;
+
+    private final MechanismLigament2d mechBaseLeft = mechRoot.append(
+            new MechanismLigament2d("ArmBaseLeft", kBaseLength.in(Meters)/2, 180, kMechWidth, kMechBaseColor));
+    private final MechanismLigament2d mechArmLeft = mechBaseLeft.append(
+            new MechanismLigament2d("ArmLeft", kPivotToWheels.in(Meters), -kDefaultArmDeg, kMechWidth, kMechBaseColor));
+    private final MechanismLigament2d mechBaseRight = mechRoot.append(
+            new MechanismLigament2d("BaseRight", kBaseLength.in(Meters)/2, 0, kMechWidth, kMechBaseColor));
+    private final MechanismLigament2d mechArmRight = mechBaseRight.append(
+            new MechanismLigament2d("ArmRight", kPivotToWheels.in(Meters), kDefaultArmDeg, kMechWidth, kMechBaseColor));
+    
+    private final MechanismLigament2d setpointBaseLeft = mechRoot.append(
+            new MechanismLigament2d("setpointArmBaseLeft", kBaseLength.in(Meters)/2, 180, kSetpointWidth, kSetpointBaseColor));
+    private final MechanismLigament2d setpointArmLeft = setpointBaseLeft.append(
+            new MechanismLigament2d("setpointArmLeft", kPivotToWheels.in(Meters), -kDefaultArmDeg, kSetpointWidth,
+                    kSetpointBaseColor));
+    private final MechanismLigament2d setpointBaseRight = mechRoot.append(
+            new MechanismLigament2d("setpointArmBaseRight", kBaseLength.in(Meters)/2, 0, kSetpointWidth, kSetpointBaseColor));
+    private final MechanismLigament2d setpointArmRight = setpointBaseRight.append(
+            new MechanismLigament2d("setpointArmRight", kPivotToWheels.in(Meters), kDefaultArmDeg, kSetpointWidth,
+                    kSetpointBaseColor));
+    
+    SingleJointedArmSim shooterArmSim = new SingleJointedArmSim(
+        LinearSystemId.createSingleJointedArmSystem(
+            DCMotor.getKrakenX60(1),
+            kMomentOfInertia.in(KilogramSquareMeters),
+            kGearRatio
+        ),
+        DCMotor.getKrakenX60(1),
+        kGearRatio,
+        kArmLength.in(Meters),
+        kOutAngle.in(Radians),
+        kInAngle.in(Radians),
+        false,//TODO:Include gravity?
+        kInAngle.in(Radians));
+
+
+    DCMotorSim motorSim = new DCMotorSim(
+        LinearSystemId.createDCMotorSystem(
+            DCMotor.getKrakenX60(1),
+            kMomentOfInertia.in(KilogramSquareMeters),
+            kGearRatio
+        ),
+        DCMotor.getKrakenX60(1)
+    );
+    
+    public void visualizeState(Angle funnelAngle) {
+        mechArmLeft.setAngle(180 + (kDefaultArmDeg - funnelAngle.in(Degrees)));
+        mechArmRight.setAngle(180 - (kDefaultArmDeg - funnelAngle.in(Degrees)));
+    }
+    
+    public void visualizeSetpoint(double targetVoltage) {
+        setpointArmLeft.setAngle(180 + (kDefaultArmDeg - ((targetVoltage > 0) ? kInAngle.in(Degrees) : kOutAngle.in(Degrees))));
+        setpointArmRight.setAngle(180 - (kDefaultArmDeg - ((targetVoltage > 0) ? kInAngle.in(Degrees) : kOutAngle.in(Degrees))));
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        TalonFXSimState motorSimState = leftMotor.getSimState();
+        motorSimState.Orientation =  ChassisReference.Clockwise_Positive;//TODO: Fix, idk what it means
+
+        motorSimState.setSupplyVoltage(leftMotor.getSupplyVoltage().getValue());//TODO: Add friction? Also, idk that the voltage should be accessed like this
+        motorSim.setInputVoltage(motorSimState.getMotorVoltage());
+
+        motorSim.update(0.02);
+
+        motorSimState.setRawRotorPosition(motorSim.getAngularPositionRotations() * kGearRatio);
+        motorSimState.setRotorVelocity(motorSim.getAngularVelocityRPM() / 60  * kGearRatio);
+        //                                           shaft RPM --> rotations per second --> motor rotations per second
+        double voltage = motorSim.getInputVoltage();
+        shooterArmSim.setInput(voltage);
+		shooterArmSim.update(0.02);
     }
 }
