cf4afaa91bb88f1671037a7dbe17b2e0702c0611
4512 Otter Chaos <otterchaos4512@gmail.com>
2025-12-16 16:15:29 -0800

    Shooter Wheels Logging

--- src/main/java/frc/robot/subsystems/ShooterWheel/ShooterWheels.java
+++ src/main/java/frc/robot/subsystems/ShooterWheel/ShooterWheels.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.ShooterWheel;
 
+import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.VelocityDutyCycle;
@@ -7,7 +8,11 @@ import com.ctre.phoenix6.hardware.TalonFX;
 
 import static edu.wpi.first.units.Units.*;
 
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -18,7 +23,11 @@ public class ShooterWheels extends SubsystemBase{
     private TalonFX leftMotor = new TalonFX(kLeftMotorID);
     private TalonFX rightMotor = new TalonFX(kRightMotorID);
 
+    private final StatusSignal<Angle> positionStatus = leftMotor.getPosition();
     private final StatusSignal<AngularVelocity> velocityStatus = leftMotor.getVelocity();
+    private final StatusSignal<Voltage> voltageStatus = leftMotor.getMotorVoltage();
+    private final StatusSignal<Current> statorStatus = leftMotor.getStatorCurrent();
+
 
     private final VelocityDutyCycle velocityRequest = new VelocityDutyCycle(0);
 
@@ -28,6 +37,34 @@ public class ShooterWheels extends SubsystemBase{
         leftMotor.getConfigurator().apply(kConfig);
         rightMotor.getConfigurator().apply(kConfig);
         rightMotor.setControl(new Follower(leftMotor.getDeviceID(), true));
+
+        SmartDashboard.putData("Shooter Wheels/Subsystem", this);
+    }
+
+    @Override
+    public void periodic() {
+        BaseStatusSignal.refreshAll(positionStatus, velocityStatus, voltageStatus, statorStatus);
+        log();
+    }
+
+    public Angle getAngle() {
+        return positionStatus.getValue();
+    }
+
+    public AngularVelocity getVelocity(){
+        return velocityStatus.getValue();
+    }
+    
+    public AngularVelocity getTargetVelocity() {
+        return targetVelocity;
+    }
+
+    public Voltage getVoltage(){
+        return voltageStatus.getValue();
+    }
+
+    public Current getCurrent(){
+        return statorStatus.getValue();
     }
 
     public void setVoltage(double voltage) {
@@ -39,12 +76,8 @@ public class ShooterWheels extends SubsystemBase{
         targetVelocity = velocity;
     }
 
-    public AngularVelocity getAngularVelocity(){
-        return velocityStatus.getValue();
-    }
-
     public boolean upToSpeed() {
-        return Math.abs(targetVelocity.in(RPM) - getAngularVelocity().in(RPM)) < kVelocityTolerance.in(RPM);
+        return Math.abs(targetVelocity.in(RPM) - getVelocity().in(RPM)) < kVelocityTolerance.in(RPM);
     }
 
     public Command setVoltageC(double voltage) {
@@ -57,5 +90,13 @@ public class ShooterWheels extends SubsystemBase{
 
     public Trigger upToSpeedT() {
         return new Trigger(()-> upToSpeed()).debounce(kDebounceTime);
+    }
+
+    public void log(){
+        SmartDashboard.putNumber("Shooter Wheels/Angle Degrees", getAngle().in(Degrees));
+        SmartDashboard.putNumber("Shooter Wheels/RPM", getVelocity().in(RPM));
+        SmartDashboard.putNumber("Shooter Wheels/Target RPM", getTargetVelocity().in(RPM));
+        SmartDashboard.putNumber("Shooter Wheels/Voltage", getVoltage().in(Volts));
+        SmartDashboard.putNumber("Shooter Wheels/Current", getCurrent().in(Amps));
     }
 }
